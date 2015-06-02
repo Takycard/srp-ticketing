90ef3517d8f046455017103fd4efa90494806613
raphael-houtin <raphael.houtin@gmail.com>
2015-06-02 09:55:20 +0200

    Bug fixed and adding status

--- app/scripts/controllers/demandescpm.js
+++ app/scripts/controllers/demandescpm.js
@@ -8,37 +8,52 @@
  * Controller of the srpTicketingApp
  */
 angular.module('srpTicketingApp')
-.controller('DemandescpmCtrl', ['$scope', 'srpCat', 'srpSsCat',
-            function ($scope, srpCat, srpSsCat) {
+.controller('DemandescpmCtrl', ['$scope', 'srpStatut', 'srpCat',
+            'srpSsCat',
+            function ($scope, srpStatut, srpCat, srpSsCat) {
 
                 $scope.userChoice = false;
 
                 // create a synchronized array for category
+                $scope.statuts = srpStatut;
+
+                // create a synchronized array for category
                 $scope.cats = srpCat;
-                $scope.cat = "";
 
                 // create a synchronized array for category
                 $scope.ssCats = srpSsCat;
-                $scope.ssCat = "";
 
 //                $scope.ssCat = [
 //                    'Test',
 //                    'Blabla'
 //                ];
-                $scope.addCat = function() {
-                    $scope.cats.$add({
-                        content: $scope.cat
+
+                $scope.addStatut = function(statut) {
+                    $scope.statuts.$add({
+                        content: statut
                         });
                 }
 
-                $scope.addSsCat = function() {
+                $scope.addCat = function(cat) {
+                    $scope.cats.$add({
+                        content: cat
+                        });
+                }
+
+                $scope.addSsCat = function(ssCat) {
                     $scope.ssCats.$add({
-                        content: $scope.ssCat
+                        content: ssCat
                     });
                 }
             }
         ])
 
+.factory("srpStatut", ["$firebaseArray", function($firebaseArray) {
+    var ref = new Firebase("https://srp-ticketing.firebaseio.com/statut");
+
+    return $firebaseArray(ref);
+}
+                    ])
 .factory("srpCat", ["$firebaseArray", function($firebaseArray) {
     var ref = new Firebase("https://srp-ticketing.firebaseio.com/category");
 
